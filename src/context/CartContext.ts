62af0566533139a1bec91a6d61b994327011f521
Exanderal <maksymilian.andrzejewski@gmail.com>
2020-10-26 18:40:00 +0100

    Typescript improvement

--- src/context/CartContext.ts
+++ src/context/CartContext.ts
@@ -1,3 +1,9 @@
 import { createContext } from "react";
+interface ICartContext {
+  quantity: number;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
+  productQuantity: number;
+  setProductQuantity: React.Dispatch<React.SetStateAction<number>>;
+}
 
-export const CartContext = createContext<any>({});
+export const CartContext = createContext({} as ICartContext);
